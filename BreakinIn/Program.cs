0829c44860ee335959d8f7447f44acd61dd11dc2
DartPower <2005369+DartPower@users.noreply.github.com>
2021-05-29 22:39:01 +0300

    Better parse external IP Address

--- BreakinIn/Program.cs
+++ BreakinIn/Program.cs
@@ -2,17 +2,17 @@
 
 namespace BreakinIn
 {
-    public class Program
-    {
-        static void Main(string[] args)
-        {
-            var server = new TSBOServer();
-            var result = server.Run(JSONConfig.Inst.external_ip);
-            if (!result)
-            {
-                Console.WriteLine("Press any key to continue...");
-                Console.ReadKey();
-            }
-        }
-    }
+	public class Program
+	{
+		static void Main(string[] args)
+		{
+			var server = new TSBOServer();
+			var result = server.Run(IPHelper.GetExternalIPAddress().ToString());
+			if (!result)
+			{
+				Console.WriteLine("Press any key to continue...");
+				Console.ReadKey();
+			}
+		}
+	}
 }
